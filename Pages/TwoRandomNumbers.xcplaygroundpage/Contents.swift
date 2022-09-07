1ae69315420081297effc978e243100130b22eb7
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-07 11:39:11 +0200

    Second better solution for generate random number done.

--- Pages/TwoRandomNumbers.xcplaygroundpage/Contents.swift
+++ Pages/TwoRandomNumbers.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ randomNumbers(number: 190) et renvoyer un chiffre aléatoire en 100...199
 
 */
 
-func randomSecondNumbers(_ number: String) -> String {
+func randomSecondNumbersA(_ number: String) -> String {
 	var arrayNumber = [Character]()
 
 	var firstNumber = ""
@@ -36,6 +36,27 @@ func randomSecondNumbers(_ number: String) -> String {
 	return "\(firstNumber)\(secondNumbers.randomElement() ?? 0)"
 }
 
-print(randomSecondNumbers("300"))
+print(randomSecondNumbersA("300"))
+
+//Better solution
+func randomSecondNumbersB(_ number: Int) -> String {
+
+	if number < 100 || number > 999 {
+		return "Error, \(number) isn't a hundred number"
+	}
+
+	let divisedNumber = number / 100
+	let hundredNumbers = Int.random(in: 0...99)
+
+	let finalNumber: String = "\(divisedNumber)\(hundredNumbers)"
+
+	if hundredNumbers < 10 {
+		return "\(divisedNumber)0\(hundredNumbers)"
+	}
+	return finalNumber
+}
+
+print(randomSecondNumbersB(400))
+
 
 //: [Next](@next)
